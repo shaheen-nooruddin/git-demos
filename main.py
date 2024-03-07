2c5db2800f57cfefd4b667d1c96d8631ba149703
shahe <shaheennooruddin8@gmail.com>
2024-03-07 15:03:57 +0000

    Added a new variable

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello world")
-
 name = input("Enter your name")
 print("welcome to python!" + name)
+
+age = input(int("Enter your age"))
+print("Welcome user you are" +age + "years old")
+
+
